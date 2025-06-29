42f2187634fd3fde7a499fe3aea39d401770e65c
ckdaas <901291+ckdaas@users.noreply.github.com>
2025-06-29 16:37:47 +1100

    remove tilde on blank lines

--- init.lua
+++ init.lua
@@ -52,6 +52,7 @@ vim.opt.splitbelow = true
 --  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.fillchars = { eob = ' ' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
